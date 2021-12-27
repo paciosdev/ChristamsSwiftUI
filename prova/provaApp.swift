a2165593f69142a4c33354e41a7e8526b7130327
Francesco Paciello <kekkopacio@gmail.com>
2021-12-27 18:18:12 +0100

    Initial Commit

---
+++ prova/provaApp.swift
@@ -0,0 +1,17 @@
+//
+//  provaApp.swift
+//  prova
+//
+//  Created by Kekko Paciello on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct provaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
