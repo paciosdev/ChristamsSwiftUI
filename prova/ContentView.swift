8bd648071b9832822189920b2c1a4d21b5a211c3
Francesco Paciello <kekkopacio@gmail.com>
2022-01-08 02:25:55 +0100

    Tolto il private dalle variabili @State

--- prova/ContentView.swift
+++ prova/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstX: CGFloat = -200
+    @State var secondX: CGFloat = 200
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("natale")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .blur(radius: 8)
+            VStack {
+                Text("🎅🏼 Merry")
+                    .foregroundColor(.white)
+                    .offset(x: firstX)
+                    
+                Text("Christmas 🎄")
+                    .bold()
+                    .foregroundColor(.white)
+                    .offset(x: secondX)
+                
+                Button {
+                    animate()
+                } label: {
+                    RoundedRectangle(cornerRadius: 12)
+                        .frame(width: 300, height: 60)
+                        .foregroundColor(.red)
+                        .overlay(Text("Click me").font(.title2).bold().foregroundColor(.white))
+                }
+                .padding(.top, 30)
+
+
+            }
+            .shadow(radius: 20)
+            .font(.largeTitle)
+            .onAppear {
+                withAnimation(Animation.easeInOut(duration: 3)){
+                    firstX = 0
+                    secondX = 0
+                }
+        }
+        }
+    }
+    
+    func animate() {
+        firstX = -200
+        secondX = 200
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            withAnimation(Animation.easeInOut(duration: 3)){
+                firstX = 0
+                secondX = 0
+            }
+        }
     }
 }
 
